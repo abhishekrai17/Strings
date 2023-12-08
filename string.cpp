63355b98dd76eb094bf77d6966f888422fa1de4f
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2023-12-08 18:27:06 +0530

    strings

---
+++ string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+//     string s;
+//     cout<<"enter the string"<<endl;
+//    getline(cin,s);
+//    int l=s.length();
+//     cout<<"the length of string is "<<l<<endl;
+//     int l1=s.size();
+//     cout<<"the size of the string is "<<l1<<endl;
+
+
+//    char a[100];
+//    cout<<"enter a character array"<<endl;
+//    cin.get(a,100);
+//    int l=0;
+//    int i=0;
+//    while(a[i]!='\0'){
+//     l++;
+//     i++;
+//    }
+//     cout<<"the length of char array is "<<l<<endl;
+
+
+   
+
+}
