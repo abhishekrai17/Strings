360ddff4330c69e2d254f521b2165a30a6e42482
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2023-12-11 10:11:13 +0530

    string array

---
+++ stringarray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+int main(){
+    
+    
+   string arr[5];
+	cout<<"Enter the elements:"<<endl;
+	for(int x = 0; x<5;x++)
+	{
+	    cin>>arr[x];
+	}
+
+	cout<<"\nString array:\n";
+	for (int x = 0; x< 5; x++) 
+		cout << arr[x] << "\n"; 
+} 
